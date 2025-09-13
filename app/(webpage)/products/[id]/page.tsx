cfa80f01a5539781689a2c986f4cf54a43c83c43
juanfe <juanfe.yepes05@gmail.com>
2025-09-12 19:44:06 -0500

    aaa2

--- app/(webpage)/products/[id]/page.tsx
+++ app/(webpage)/products/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { supabase } from "@/lib/supabaseClient";
-
+import Image from "next/image";
 interface Producto {
   id: number;
   sku: string;
@@ -49,7 +49,6 @@ export default function Page({ params }: PageProps) {
   };
   loadData();
 
-  //extractId().then(id => fetchProducto(id));
 }, [params]);
 
   if (loading) return <p>Cargando producto...</p>;
@@ -63,6 +62,12 @@ export default function Page({ params }: PageProps) {
       <p className="text-green-600 text-lg font-semibold mt-2">
         ${producto.precio.toLocaleString()}
       </p>
+      <Image
+        src="https://images.falabella.com/v3/assets/blt088e6fffbba20f16/blt4c474b53ecc2a0ac/65e93b7882d68f0bd6d20cf9/falabella.com_green_icon_mobile.svg"
+        alt="Falabellla"
+        width={120}
+        height={40}
+      />
     </div>
   );
 }
