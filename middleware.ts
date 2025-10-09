8ccc699018baf60704ab7e352a8521acd829c714
Stiven Carvajal <stivencarvajal205@gmail.com>
2025-10-08 19:16:28 -0500

    se corrigio la obtencion del token
    en el middleware

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const token = req.cookies.get("auth-token"); 
+  const token = req.cookies.get("token"); 
 
   const isAuth = !!token; // ejemplo simple
   const { pathname } = req.nextUrl;
