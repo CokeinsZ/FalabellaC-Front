fbf5d51aca6ee0c5f2ec46b09d7a89a6125ea2b7
daniel <danielespitia369@gmail.com>
2025-10-29 17:19:13 -0500

    implements midlleware

--- middleware.ts
+++ middleware.ts
@@ -11,10 +11,25 @@ export function middleware(req: NextRequest) {
   if (pathname.startsWith("/cart") && !isAuth) {
     return NextResponse.redirect(new URL("/home", req.url));
   }
+  if (pathname.startsWith("/seller")) {
+  const userId = req.cookies.get("user_id");
+  const nit = req.cookies.get("nit");
+  if (!userId || !nit) {
+    return NextResponse.redirect(new URL("/Vende-en-Falabella", req.url));
+  }
+}
+
+  if (pathname.startsWith("/Vende-en-Falabella")) {
+  const userId = req.cookies.get("user_id");
+  const nit = req.cookies.get("nit");
+  if (userId || nit) {
+    return NextResponse.redirect(new URL("/seller", req.url));
+  }
+}
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/cart/:path*"], 
+  matcher: ["/cart/:path*", "/seller/:path*", "/Vende-en-Falabella/:path*"],
 };
